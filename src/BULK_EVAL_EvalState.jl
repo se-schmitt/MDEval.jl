e08658d3af13af337eda247956343912ef3870b5
Sebastian <se.s.schmitt@gmail.com>
2020-11-10 09:52:15 +0100

    Update BULK_EVAL_EvalState.jl
    Minor Modification

--- src/BULK_EVAL_EvalState.jl
+++ src/BULK_EVAL_EvalState.jl
@@ -33,8 +33,8 @@ function EvalState(subfolder::Array{String,1}, inpar::input_struct)
 
     state = state_info(T.val,p.val,ρ.val,natoms,mass)
 
-    setTDM = set_TDM(folder,subfolder,false,0.5,0.4,NaN,"","","",inpar.n_boot)
-    setTDM.tskip = 20
+    setTDM = set_TDM(folder,subfolder,false,NaN,NaN,NaN,"","","",inpar.n_boot)
+    # setTDM.tskip = 20
     setTDM.cutcrit = 0.4
 
     # Transport Properties
