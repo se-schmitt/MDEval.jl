39ea49b028f2e56cfaf0d7dec66c366c8db9f21c
Sebastian Schmitt <schmitts@rhrk.uni-kl.de>
2022-03-08 16:20:15 +0100

    Update EvalNEMDShear.jl
    Revision of the nemd shear evaluation

--- src/EvalNEMDShear.jl
+++ src/EvalNEMDShear.jl
@@ -24,89 +24,47 @@ function EvalNEMDShear(subfolder,inpar)
     # Average Thermodynamic Properties
     T, p, ρ, Etot, Ekin, Epot, c, pyz = ave_thermo(info; is_nemd=true)
 
-    # Load data
+    # Load profile data
     ts_add = 0
-    data = []
+    prof = []
     for f in list[startswith.(list,"vy_profile")]
-        data, no_chunks, n_steps = read_profile1D("$(info.folder)/$f",prof,ts_add)
+        prof, no_chunks, n_steps = read_profile1D("$(info.folder)/$f",prof,ts_add)
         ts_add = prof.timestep[end]
     end
-    what = data.timestep .>= info.n_equ
-    vy_mean = return_vy_mean(data,what,n_steps,no_chunks)
-    x = vy_mean[:,2]
-    y = vy_mean[:,4]
-    a,b = hcat(fill!(similar(x), 1), x) \ y
-    x_0 = ones(size(x,1))
 
-    y_predict=a*x_0+b*x
-    # Sum of Squares Total, Sum of Squares Regression
-    SST = sum((y .- mean(y)).^2)
-    SSR = sum((y_predict .- mean(y)).^2)
-    r_square = SSR / SST
-    r_squared = single_dat(r_square,0,0)
-
-
-    timestep_equend = data.timestep[what]
-    filename2 = "$(info.folder)/in.Bulk_NVT_sllod"
-    N_mol = read_inputfile(filename2)
-
-    L = ((N_mol*info.molmass*1e24)/(ρ.val*6.02214076e23))^(1/3)
-    s_rate = 10^12*b/L #10^15 for ReaxFF 10^12 for others
+    # Load thero data
     dat = load_thermo(info, is_nemd="shear")
     if (reduced_units)      factor_p = 1
     elseif !(reduced_units) factor_p = 0.1 end
 
-    η_vec = -(dat.pyz.*factor_p)*1e6/s_rate
+    # Calculate velocity gradient
+    xbins = mean(prof.x,dims=1)[:]
+    vybins = mean(prof.vy,dims=1)[:]
+    vybins_std = std(prof.vy,dims=1)[:]
+    dvdx = ([ones(length(xbins),1) xbins] \ vybins)[2]
 
-    η = single_dat(mean(η_vec), block_average(η_vec; M_block=100)[1], block_average(η_vec; M_block=100)[2])
-    figure()
-    xlabel(L"x")
-    ylabel(L"vy")
-    plot(x,y, color="gray", linestyle=":", linewidth=0.1)
-    title("Fit",fontsize=10)
-    legend(loc="upper left")
-    tight_layout()
-    filename3 = "$(info.folder)/vy_plot.pdf"
-    savefig(filename3)
+    # L = ...                                   →   JENS (aus info Datei)
+    s_rate = 10^12*dvdx/L                   #   →   JENS (Einheiten überprüfen (sollte 1/s sein, [dvdx]=Å/s, [L]=Å) und reduzierte Einheiten hinzufügen)
+
+    # Calculation of viscosity and its uncertainties
+    η_vec = -(dat.pyz.*factor_p)*1e6/s_rate
+    η_ave = mean(η_vec)
+    η_std, η_err = block_average(η_vec; M_block=100)
+
+    # Save viscosity
+    η = single_dat(η_ave, η_std, η_err)
 
     # Output results
-    res = results_struct_nemd(T, p, ρ, x, Etot, Ekin, Epot, pyz, η, s_rate, r_squared, [])
+    res = results_struct_nemd(T, p, ρ, [1.0], Etot, Ekin, Epot, η, s_rate, [])
     OutputResultNEMD(res, info.folder)
-end
 
-
-
-
-## Subfunctions
-function return_vy_mean(data,what,n_steps,no_chunks)
-
-    id_chunk_mat = ones(1,n_steps)
-    x_mat = ones(1,n_steps)
-    Ncount_mat = ones(1,n_steps)
-    vy_mat = ones(1,n_steps)
-    vy_mean = ones(no_chunks,6)
-
-
-    #loop to generate vy_mean matrix for n_equ:end
-    for i = 1 : no_chunks
-        for j = 1 : n_steps
-            id_chunk_mat[j] = data.id_chunk[j,i]
-            x_mat[j] = data.x[j,i]
-            Ncount_mat[j] = data.Ncount[j,i]
-            vy_mat[j] = data.vy[j,i]
-        end
-        id_chunk_vec = single_dat(mean(id_chunk_mat[what]), block_average(id_chunk_mat[what])[1], block_average(id_chunk_mat[what])[2])
-        x_vec = single_dat(mean(x_mat[what]), block_average(x_mat[what])[1], block_average(x_mat[what])[2])
-        Ncount_vec = single_dat(mean(Ncount_mat[what]), block_average(Ncount_mat[what])[1], block_average(Ncount_mat[what])[2])
-        vy_vec = single_dat(mean(vy_mat[what]), block_average(vy_mat[what])[1], block_average(vy_mat[what])[2])
-
-        #stores mean values for n_equ:end for col1: id_chunk, col2: x, col3:N_count, col4-6 vy
-        vy_mean[i,1] = round(id_chunk_vec.val)
-        vy_mean[i,2] = round(x_vec.val,digits=4)
-        vy_mean[i,3] = round(Ncount_vec.val,digits=0)
-        vy_mean[i,4] = round(vy_vec.val,digits=4)
-        vy_mean[i,5] = round(vy_vec.std,digits=4)
-        vy_mean[i,6] = round(vy_vec.err,digits=4)
-    end
-    return vy_mean
+    # Figures ------------------------------------------------------------------
+    # Figure of velocity profile
+    figure()
+    title("Mean velocity profile of simulation box")
+    if reduced_units    xlabel(L"x^*");                 ylabel(L"v_y^*")
+    else                xlabel(string(L"x"," / Å"));    ylabel(string(L"v_y"," / Å/ps"))    end
+    errorbar(xbins, vybins, yerr=vybins_std, fmt="bo-", capsize=2)
+    tight_layout()
+    savefig("$(info.folder)/vy_profile.pdf")
 end
